787fd6f9f3b528d26a3c5789ed90f4f1ca8f5d0b
feiyin <fy@yehd.xyz>
2026-05-13 16:57:38 +0800

    testpass

--- lib/codec/build.sh
+++ lib/codec/build.sh
@@ -17,12 +17,12 @@ cd ./build
 # emmake make -j 4
 # mv ../bin/prod.js ../bin/baseline.asm.js
 
-node ../tool/compile.js
-node ../tool/wrapper.js ../bin/baseline.wasm.js baseline.wasm
+# node ../tool/compile.js
+# node ../tool/wrapper.js ../bin/baseline.wasm.js baseline.wasm
 # node ../tool/wrapper.js ../bin/baseline.asm.js baseline.asm
 
 # all decode library
-node ../tool/compile.js wasm all
+# node ../tool/compile.js wasm all
 emcmake cmake . -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 emmake make -j 4
 mv ./bin/prod.js ./bin/all.wasm.js
@@ -36,17 +36,19 @@ mv ./bin/prod.js ./bin/all.wasm.js
 node ./tool/wrapper.js ../bin/all.wasm.js all.wasm
 # node ../tool/wrapper.js ../bin/all.asm.js all.asm
 
+cp ./combine/prod.all.wasm.combine.js ../../example/prod.all.wasm.combine.js
+
 ## h265 decode library
-node ../tool/compile.js wasm h265
-emcmake cmake ..
-emmake make -j 4
-mv ../bin/prod.js ../bin/h265.wasm.js
+# node ../tool/compile.js wasm h265
+# emcmake cmake ..
+# emmake make -j 4
+# mv ../bin/prod.js ../bin/h265.wasm.js
 
 # node ../tool/compile.js asm h265
 # emcmake cmake ..
 # emmake make -j 4
 # mv ../bin/prod.js ../bin/h265.asm.js
 
-node ../tool/compile.js
-node ../tool/wrapper.js ../bin/h265.wasm.js h265.wasm
+# node ../tool/compile.js
+# node ../tool/wrapper.js ../bin/h265.wasm.js h265.wasm
 # node ../tool/wrapper.js ../bin/h265.asm.js h265.asm
